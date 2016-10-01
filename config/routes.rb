5dd4696c7e69b313f3cc6124d312d31fe2b846b5
TrongAnPham <1312024@student.hcmus.edu.vn>
2016-10-01 22:38:24 +0700

    Revert "Chức năng bình luận + chỉnh sửa"
    This reverts commit 2b48c39ce7ea5f72007e4b21499b5f4629e4295a.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
  
-  resources :articles do
-  resources :comments
-end
+  resources :articles
  
   root 'welcome#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161001111216) do
+ActiveRecord::Schema.define(version: 20161001103522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,14 +22,4 @@ ActiveRecord::Schema.define(version: 20161001111216) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.string   "commenter"
-    t.text     "body"
-    t.integer  "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
-  end
-
-  add_foreign_key "comments", "articles"
 end
