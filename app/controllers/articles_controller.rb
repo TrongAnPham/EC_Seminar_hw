c0d53e7963fe07ec15f0b0d973bedb67bcd79aa6
TrongAnPham <1312024@student.hcmus.edu.vn>
2016-10-01 17:55:57 +0700

    Update blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,12 @@ def show
     @article = Article.find(params[:id])
   end
 
- def new
+def new
   @article = Article.new
+end
+ 
+def edit
+  @article = Article.find(params[:id])
 end
  
 def create
@@ -18,6 +22,16 @@ def create
     redirect_to @article
   else
     render 'new'
+  end
+end
+ 
+def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
   end
 end
  
